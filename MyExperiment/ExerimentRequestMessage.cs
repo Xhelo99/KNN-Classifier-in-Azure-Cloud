08bef775684fb6c1c7587818c0d2cf1ed8460225
Geni <mariglenxhelo09@gmail.com>
2024-08-12 14:48:00 +0200

    Describe the parameters.

--- MyExperiment/ExerimentRequestMessage.cs
+++ MyExperiment/ExerimentRequestMessage.cs
@@ -7,11 +7,22 @@ namespace MyExperiment
 {
     internal class ExerimentRequestMessage : IExerimentRequest
     {
+        // Unique identifier for the experiment.
         public string ExperimentId { get; set; }
+
+        // Name of the input file used in the experiment.
         public string InputFile { get; set; }
+
+        // Name of the experiment.
         public string Name { get; set; }
+
+        // A brief description of the experiment.
         public string Description { get; set; }
+
+        // Unique identifier for the message associated with the experiment request.
         public string MessageId { get; set; }
+
+        // Receipt of the message, used for confirming the message delivery.
         public string MessageReceipt { get; set; }
     }
 }
